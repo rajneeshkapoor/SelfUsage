0034d367df3f49e955189856faca30bef2fee03b
root <root@localhost.localdomain>
2018-05-21 14:04:41 -0400

    Script modified to create on TVM without going into console

--- Scripts/createTvmOnKVM/setTVMOnKVM.sh
+++ Scripts/createTvmOnKVM/setTVMOnKVM.sh
@@ -1,3 +1,21 @@
+setvars()
+{
+	clear
+	iFlag="y"
+	USER_DATA="/tmp/user-data"
+	META_DATA="/tmp/meta-data"
+	TVAULT_ISO="/tmp/tvault"
+	MEM=6000
+	CPUs=4
+	BRIDGE=`brctl show | awk '{print $1}' | head -2 | tail -1`
+	imageTargetLoc="/var/lib/libvirt/images"
+	tempFile="/tmp/temp.txt"
+	read -p "Image tar zip absolute path : " imagePath
+	read -p "TVM Machine Count : " ipCount
+	read -p "TVM Machine Hostname (Multiple TVMs will be appended with machine count): " tvmName
+	validate
+}
+
 validate()
 {
 	if [ ${ipCount} -ne 1 -a ${ipCount} -ne 3 ]; then
@@ -7,7 +25,6 @@ validate()
 		return
 	fi
 	if [ -f ${imagePath} ]; then
-		cp ${imagePath} ${imageTargetLoc}
 		echo ${imagePath} >> ${tempFile}
 		if [ `grep "ha" ${tempFile}` ]; then
 			imageFile=`ls ${imagePath} | awk -F'/' '{print $NF}' | cut -d"." -f1-5`
@@ -21,21 +38,12 @@ validate()
 		iFlag="n"
 	fi
 }
+
 setips()
 {
 	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
 		read -p "Enter IP ${iTemp}: " ip[${iTemp}]
 	done
-}
-setvars()
-{
-	clear
-	iFlag="y"
-	imageTargetLoc="/var/lib/libvirt/images"
-	tempFile="/tmp/temp.txt"
-	read -p "Image tar zip absolute path : " imagePath
-	read -p "TVM Machine Count : " ipCount
-	validate
 }
 
 showvars()
@@ -51,10 +59,71 @@ showvars()
 
 extractAndCopy()
 {
+	cp ${imagePath} ${imageTargetLoc}
 	cd ${imageTargetLoc}
 	tar xvzf ${imageFile}.tar.gz
 	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
 		cp ${imageFile} ${imageFile}_${iTemp}
+	done
+}
+
+setUserData()
+{
+	cat > ${USER_DATA}_${iTemp} << _EOF_
+preserve_hostname: False
+hostname: ${tvmName}_${iTemp}
+# Remove cloud-init when finished with it
+runcmd:
+  - [ yum, -y, remove, cloud-init ]
+# Configure where output will go
+output: 
+  all: ">> /var/log/cloud-init.log"
+# configure interaction with ssh server
+ssh_svcname: ssh
+ssh_deletekeys: True
+ssh_genkeytypes: ['rsa', 'ecdsa']
+# Install my public ssh key to the first user-defined user configured 
+# in cloud.cfg in the template (which is centos for CentOS cloud images)
+ssh_authorized_keys:  
+  - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfNePSyLNJ7UOvThXb75fyEh6GbYhHHkAkqvTZexGFC2WwPi0aPNRxMlf8pSHEHtowFz90l+KFqQuwurSqGfCCBM/PvhFaCFcUASVrcNPO8SCt8nDoDM5Ir6lrv5qd/WX63ODsQzs/HIC3TVK75nxIMSI46Yde1v2Kha3pe0JD7Tt0NdJFVq2eTsGaK9oMeuZcpR2OkfQDdGb4YeDshrvU011XEMez8hG2mqi39hKJIqZcvq/Z0h7ECIp/nP+ypGOYwWDd/Sab5/UkluqkL3LUK69XKrrdPjlw/COe5qZ2x8Gi86MneELRoOYb4iEfA6f1m/PBvcB9SeNQyiLzDzSB root@localhost.localdomain
+_EOF_
+}
+
+setMetaData()
+{
+	cat > ${META_DATA}_${iTemp} << _EOF_
+instance-id: ${tvmName}_${iTemp}
+network-interfaces: |
+auto ens3
+iface ens3 inet static
+address ${ip[${iTemp}]}
+netmask 255.255.255.0
+gateway 192.168.1.1
+dns-nameservers 192.168.1.1 8.8.8.8
+local-hostname: ${tvmName}_${iTemp}
+_EOF_
+}
+
+setISO()
+{
+	genisoimage -output ${TVAULT_ISO}_${iTemp}.iso -volid cidata -joliet -rock ${USER_DATA}_${iTemp} ${META_DATA}_${iTemp}
+}
+
+setDataFiles()
+{
+	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
+		setUserData
+		setMetaData
+		setISO
+	done
+}
+
+createVMs()
+{
+	cd ${imageTargetLoc}
+	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
+		virt-install --import --name ${tvmName}_${iTemp} --memory $MEM --vcpus $CPUs --disk ${imageFile}_${iTemp},format=qcow2,bus=virtio --disk ${TVAULT_ISO}_${iTemp}.iso,device=cdrom --network  bridge=virbr0,model=virtio --os-type=linux --os-variant=rhel7 --noautoconsole
+		virsh change-media ${tvmName}_${iTemp} hda --eject --config
 	done
 }
 
@@ -77,6 +146,9 @@ do
 	fi
 done
 
-extractAndCopy
+setDataFiles
+#extractAndCopy
+createVMs
+
 
 echo "Finally..."
