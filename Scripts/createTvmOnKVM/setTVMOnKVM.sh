f9fdf164a67a622773a7ba183284eeff54741477
root <root@localhost.localdomain>
2018-05-25 08:00:54 -0400

    Remove not required script

--- Scripts/createTvmOnKVM/setTVMOnKVM.sh
+++ Scripts/createTvmOnKVM/setTVMOnKVM.sh
@@ -1,3 +1,4 @@
+#set -x
 setvars()
 {
 	clear
@@ -14,6 +15,13 @@ setvars()
 	read -p "TVM Machine Count : " ipCount
 	read -p "TVM Machine Hostname (Multiple TVMs will be appended with machine count): " tvmName
 	validate
+}
+
+cleanUp()
+{
+	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
+		rm -f ${TVAULT_ISO}_${iTemp}.iso ${USER_DATA}_${iTemp} ${META_DATA}_${iTemp}
+	done
 }
 
 validate()
@@ -69,44 +77,31 @@ extractAndCopy()
 
 setUserData()
 {
-	cat > ${USER_DATA}_${iTemp} << _EOF_
+	cat > ${USER_DATA} << _EOF_
 preserve_hostname: False
 hostname: ${tvmName}_${iTemp}
-# Remove cloud-init when finished with it
-runcmd:
-  - [ yum, -y, remove, cloud-init ]
-# Configure where output will go
-output: 
-  all: ">> /var/log/cloud-init.log"
-# configure interaction with ssh server
-ssh_svcname: ssh
-ssh_deletekeys: True
-ssh_genkeytypes: ['rsa', 'ecdsa']
-# Install my public ssh key to the first user-defined user configured 
-# in cloud.cfg in the template (which is centos for CentOS cloud images)
-ssh_authorized_keys:  
-  - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfNePSyLNJ7UOvThXb75fyEh6GbYhHHkAkqvTZexGFC2WwPi0aPNRxMlf8pSHEHtowFz90l+KFqQuwurSqGfCCBM/PvhFaCFcUASVrcNPO8SCt8nDoDM5Ir6lrv5qd/WX63ODsQzs/HIC3TVK75nxIMSI46Yde1v2Kha3pe0JD7Tt0NdJFVq2eTsGaK9oMeuZcpR2OkfQDdGb4YeDshrvU011XEMez8hG2mqi39hKJIqZcvq/Z0h7ECIp/nP+ypGOYwWDd/Sab5/UkluqkL3LUK69XKrrdPjlw/COe5qZ2x8Gi86MneELRoOYb4iEfA6f1m/PBvcB9SeNQyiLzDzSB root@localhost.localdomain
 _EOF_
 }
 
 setMetaData()
 {
-	cat > ${META_DATA}_${iTemp} << _EOF_
+	cat > ${META_DATA} << _EOF_
 instance-id: ${tvmName}_${iTemp}
 network-interfaces: |
-auto ens3
-iface ens3 inet static
-address ${ip[${iTemp}]}
-netmask 255.255.255.0
-gateway 192.168.1.1
-dns-nameservers 192.168.1.1 8.8.8.8
-local-hostname: ${tvmName}_${iTemp}
+  auto ens3
+  iface ens3 inet static
+  address ${ip[${iTemp}]}
+  netmask 255.255.0.0
+  gateway 192.168.1.1
+  dns-nameservers 192.168.1.1 8.8.8.8
+hostname: ${tvmName}_${iTemp}
 _EOF_
 }
 
 setISO()
 {
-	genisoimage -output ${TVAULT_ISO}_${iTemp}.iso -volid cidata -joliet -rock ${USER_DATA}_${iTemp} ${META_DATA}_${iTemp}
+	genisoimage -output ${TVAULT_ISO}_${iTemp}.iso -volid cidata -joliet -rock ${USER_DATA} ${META_DATA}
+	cp ${USER_DATA} ${USER_DATA}_${iTemp}; cp ${META_DATA} ${META_DATA}_${iTemp}
 }
 
 setDataFiles()
@@ -122,7 +117,7 @@ createVMs()
 {
 	cd ${imageTargetLoc}
 	for ((iTemp=1;iTemp <= ${ipCount};iTemp++)); do
-		virt-install --import --name ${tvmName}_${iTemp} --memory $MEM --vcpus $CPUs --disk ${imageFile}_${iTemp},format=qcow2,bus=virtio --disk ${TVAULT_ISO}_${iTemp}.iso,device=cdrom --network  bridge=virbr0,model=virtio --os-type=linux --os-variant=rhel7 --noautoconsole
+		virt-install --import --name ${tvmName}_${iTemp} --memory $MEM --vcpus $CPUs --disk ${imageFile}_${iTemp},format=qcow2,bus=virtio --disk ${TVAULT_ISO}_${iTemp}.iso,device=cdrom --network  bridge=virbr0,model=virtio --os-type=linux --noautoconsole
 		virsh change-media ${tvmName}_${iTemp} hda --eject --config
 	done
 }
@@ -146,9 +141,11 @@ do
 	fi
 done
 
+cleanUp
 setDataFiles
-#extractAndCopy
+extractAndCopy
 createVMs
 
 
 echo "Finally..."
+set +x
